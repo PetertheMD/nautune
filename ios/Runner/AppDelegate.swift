b681b3582731a876d2df27407aea14c2a27df7da
Ben <145032590+ElysiumDisc@users.noreply.github.com>
2025-11-02 20:29:08 -0800

    iOS Build Fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import CarPlay
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,5 +10,22 @@ import UIKit
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  override func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    if connectingSceneSession.role == .carTemplateApplication {
+      let sceneConfig = UISceneConfiguration(
+        name: "CarPlay",
+        sessionRole: connectingSceneSession.role
+      )
+      sceneConfig.delegateClass = CarPlaySceneDelegate.self
+      return sceneConfig
+    }
+    
+    return super.application(application, configurationForConnecting: connectingSceneSession, options: options)
   }
 }
